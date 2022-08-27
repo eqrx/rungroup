83ef211e8832390a5e80150eb9e46d69e4bc44f4
Alexander Sowitzki <dev@eqrx.net>
2022-08-27 21:07:12 +0200

    Update linter

--- error_test.go
+++ error_test.go
@@ -11,6 +11,7 @@
 // You should have received a copy of the GNU Affero General Public License along with this program.
 // If not, see <https://www.gnu.org/licenses/>.
 
+//nolint:goerr113
 package rungroup_test
 
 import (
